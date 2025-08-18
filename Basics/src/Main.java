8fbab3130ca4b077c40c6847ef4c998163f1bcc8
Juan Diego <diegovegaprogra@gmail.com>
2025-08-18 10:29:01 -0700

    -- basics II --

--- Basics/src/Main.java
+++ Basics/src/Main.java
@@ -1,7 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+
+
+        // VARIABLES EN JAVA
+
+        // Es un tipo de dato de 32 bits con signo para almacenar valores numéricos. Cuyo valor mínimo es -231 y el valor máximo 231-1.
+       int num = 100;
+       int num2 = 200;
+
+       // Es un tipo de dato para almacenar números en coma flotante con doble precisión de 64 bits.
+       double decimal = 16.5;
+
+       // el estring no es un tipo de dato primitivo y en inmutable
+       String s = "hola mundo";
+
+       // Sirve para definir tipos de datos booleanos. Es decir, aquellos que tienen un valor de true o false. Ocupa 1 bit de información.
+       boolean False;
+
+       // Es un tipo de datos que representa a un carácter Unicode sencillo de 16 bits.
+       char C = 'A';
+
+       // Representa un tipo de dato de 16 bits con signo. De esta manera almacena valores numéricos de -32.768 a 32.767.
+       short num1 = 1000;
+
+       // Es un tipo dato para almacenar números en coma flotante con precisión simple de 32 bits.
+       float h = 12.300f;
+
+       // Using Underscore Characters in Numeric Literals
+
+        // para separar numeros se usa el _ para que tenga una mejor imagen
+
+        long creditCardNumber = 1234_5678_9012_3456L;
+        long socialSecurityNumber = 999_99_9999L;
+        float pi =  3.14_15F;
+        long hexBytes = 0xFF_EC_DE_5E;
+        long hexWords = 0xCAFE_BABE;
+        long maxLong = 0x7fff_ffff_ffff_ffffL;
+        byte nybbles = 0b0010_0101;
+        long bytes = 0b11010010_01101001_10010100_10010010;
+
+
+
 
 
 
